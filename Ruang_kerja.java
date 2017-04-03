b5c7349b8d357f90f488c893075aef63f0187e3f
Ripan DK <ripan.dk13@gmail.com>
2017-04-03 21:09:17 +0700

    Elearning membuat class & tugas 03 april 2017

---
+++ Ruang_kerja.java
@@ -0,0 +1,25 @@
+public class Ruang_kerja {
+	String komputer;
+	String printer;
+	String telepon;
+	String meja;
+
+public void set( String komputer,String printer, String telepon, String meja){
+	this.komputer = komputer;
+	this.printer = printer;
+	this.telepon = telepon;
+	this.meja = meja;
+	}
+public void lihat(){
+	System.out.println("Alat - alat yang di gunakan di tempat kerja" + " \n");
+	System.out.println("komputer  :" + komputer);
+	System.out.println("printer   :" + printer);
+	System.out.println("telepon   :" + telepon);
+	System.out.println("meja      :" + meja);
+	}
+public static void main(String[] args) {
+	Ruang_kerja PT = new Ruang_kerja();
+	PT.set("entry", "cetak", "komunikasi", "tempat duduk");
+	PT.lihat();
+	}
+}
