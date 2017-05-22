470004d63e6f910565cbd42385ca298e4e334809
Ripan DK <ripan.dk13@gmail.com>
2017-05-22 23:01:43 +0700

    Ripan.d kusumah-14111196-Enkapintegration
    Ripan.d kusumah-14111196-Enkapintegration

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class main{
+  public static void main(String[] args){
+  Ruang_kerja data= new Ruang_kerja(" Komputer kerja","Printer warna");
+  System.out.println(" Attribute Ruang kerja :" +data.setkomputer());
+  System.out.println(" Atribute Ruang kerja :") +data.setprinter);
+}
+}
--- Ruang_kerja.java
+++ Ruang_kerja.java
@@ -1,27 +1,28 @@
-public class Ruang_kerja extends Perusahaan{
-	String komputer;
-	String printer;
-	String telepon;
-	String meja;
+public class Ruang_kerja {
 
-public void set( String komputer,String printer, String telepon, String meja){
+	private String komputer;
+
+	public String printer;
+
+
+public Ruang_kerja ( String komputer,String printer){
+
 	this.komputer = komputer;
+
 	this.printer = printer;
-	this.telepon = telepon;
-	this.meja = meja;
-	}
-public void fasilitas(){
-	super.fasilitas();
-	System.out.println("Alat - alat yang di gunakan di tempat kerja" + " \n");
-	System.out.println("komputer  :" + komputer);
-	System.out.println("printer   :" + printer);
-	System.out.println("telepon   :" + telepon);
-	System.out.println("meja      :" + meja);
+
+
 	}
 
-public static void main(String[] args) {
-	Ruang_kerja PT = new Ruang_kerja();
-	PT.set("entry", "cetak", "komunikasi", "tempat duduk");
-	PT.fasilitas();
+public String setkomputer(){
+
+	return komputer;
+
 	}
+
+
+public String setprinter(){
+  return printer
+  }
 }
+
